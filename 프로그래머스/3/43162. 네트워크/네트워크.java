acc8371a998ffe65baec2570c59f8365298c47a6
김은수 <devegoist@gmail.com>
2025-11-06 17:34:02 +0900

    [level 3] Title: 네트워크, Time: 1.40 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,46 @@
+import java.util.*;
+
 class Solution {
-    private int[][] computers;
-    private boolean[] visited;
-    
     public int solution(int n, int[][] computers) {
-        this.computers = computers;
-        this.visited = new boolean[n];
-        int count = 0;
+        // 너비우선탐색으로 풀어보자
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (computers[i][j] == 1) {
+                    if (!graph.containsKey(i)) {
+                        graph.put(i, new ArrayList<>());
+                    }
+                    graph.get(i).add(j);    
+                }
+            }
+        }
+        boolean[] visited = new boolean[n+1];
         
+        // 0, [0, 1]
+        // 1, [1, 0]
+        // 2, [2]
+        
+        int count = 0;
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs(i);
                 count++;
-            }
+                
+                Queue<Integer> queue = new LinkedList<>();
+                visited[i] = true;
+                queue.add(i);
+
+                while (!queue.isEmpty()) {
+                    int computer = queue.poll();
+                    for (int linked : graph.get(computer)) {
+                        if (!visited[linked]) {
+                            visited[linked] = true;
+                            queue.add(linked);
+                        }
+                    }
+                }  
+            }  
         }
         
         return count;
-    }
-    
-    private void dfs(int computer) {
-        visited[computer] = true;
-        
-        for (int i = 0; i < computers.length; i++) {
-            if (computers[computer][i] == 1 && !visited[i]) {
-                dfs(i);
-            }
-        }
-        
     }
 }
