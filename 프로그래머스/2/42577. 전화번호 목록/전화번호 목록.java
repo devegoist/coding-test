18508890672122eeb8935a8fc8b92dc8c4dc77ed
김은수 <devegoist@gmail.com>
2025-11-16 17:21:15 +0900

    [level 2] Title: 전화번호 목록, Time: 339.79 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,20 +2,14 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        
         boolean answer = true;
         
-        Set<String> bookSet = new HashSet<>();
-        for (String phone : phone_book) {
-            bookSet.add(phone);
-        }
-        
-        for (String phone : phone_book) {
-            for (int i = 1; i < phone.length(); i++) {
-                String prefix = phone.substring(0, i);
-                if (prefix.equals(phone)) continue;
-                if (bookSet.contains(prefix)) {
-                    return false;
-                }
+        for (int i = 0; i < phone_book.length -1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
             }
         }
         
