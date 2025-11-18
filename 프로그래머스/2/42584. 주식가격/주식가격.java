51ca708961f8431ed0ea8e18b6152a2e5c95a61b
김은수 <devegoist@gmail.com>
2025-11-18 22:24:31 +0900

    [level 2] Title: 주식가격, Time: 37.91 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,11 +3,10 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        
         Stack<Integer> stack = new Stack<>();
         
         for (int i = 0; i < prices.length; i++) {
-            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
                 int idx = stack.pop();
                 answer[idx] = i - idx;
             }
