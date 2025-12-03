2a4b61b0c75745c563e7a448b9e03d39917e35eb
김은수 <devegoist@gmail.com>
2025-12-03 17:57:17 +0900

    Time: 0 ms (100%), Space: 43 MB (14.21%) - LeetHub

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,10 +1,5 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
-            if (haystack.substring(i).startsWith(needle)) {
-                return i;
-            }
-        }
-        return -1;
+        return haystack.indexOf(needle);
     }
 }
