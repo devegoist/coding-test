09f905cc7a99792ba3544298c8d6803651b3a8c0
김은수 <devegoist@gmail.com>
2025-12-05 17:49:37 +0900

    Time: 3 ms (15.38%), Space: 42.8 MB (5.49%) - LeetHub

--- LeetCode/Easy/1556-thousand-separator/1556-thousand-separator.java
+++ LeetCode/Easy/1556-thousand-separator/1556-thousand-separator.java
@@ -1,12 +1,5 @@
 class Solution {
     public String thousandSeparator(int n) {
-        // 1234 -> 1.234
-        // 12341234 -> 12.341.234
-
-        // 12341234 % 1000 -> 12341...234
-        // 12341 % 1000 -> 12...341
-        // 12 % 1000 -> 0 ... 12
-
         if (n < 1000) {
             return String.valueOf(n);
         }
@@ -15,20 +8,20 @@ class Solution {
         while (n > 0) {
             int r = n % 1000;
             n = n / 1000;
-            list.add(String.valueOf(r));
+
+            String value = String.valueOf(r);
+            if (n > 0) {
+                if (r < 10) {
+                    value = "00" + value;
+                } else if (r < 100) {
+                    value = "0" + value;
+                }
+            }
+            
+            list.add(value);
         }
 
         String[] result = list.reversed().toArray(String[]::new);
-        for (int i = 0; i < result.length; i++) {
-            if (i > 0) {
-                if (result[i].length() < 3) {
-                    result[i] = "0" + result[i];
-                } else if (result[i].length() < 2) {
-                    result[i] = "00" + result[i];
-                }
-            }
-        }
-        
         return String.join(".", result);
     }
 }
