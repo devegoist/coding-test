54b03821d40ae7038cb5e7af560cb00b0ee6ea58
김은수 <devegoist@gmail.com>
2025-12-05 22:19:38 +0900

    Time: 52 ms (5.44%), Space: 48.8 MB (47.93%) - LeetHub

--- LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.java
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.java
@@ -3,35 +3,26 @@ class Solution {
         List<int[]> result = new ArrayList<>();
 
         // sorting
-        Arrays.sort(intervals, (a, b) -> {
-            if (a[0] == b[0]) {
-                return a[1] - b[1];
-            }
-            return a[0] - b[0];
-        });
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
         int start = intervals[0][0];
         int end = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
             System.out.println(intervals[i][0] + " " + intervals[i][1]);
-            
-            if (intervals[i][0] <= end && end <= intervals[i][1]) {
-                end = intervals[i][1];
-            } else if (intervals[i][0] <= end && intervals[i][1] <= end) {
+
+            int start2 = intervals[i][0];
+            int end2 = intervals[i][1];
+
+            if (start2 <= end) {
+                end = Math.max(end, end2);
             } else {
                 result.add(new int[]{start, end});
-                start = intervals[i][0];
-                end = intervals[i][1];
-            }
-
-            if (intervals.length - 1 == i) {
-                result.add(new int[]{start, end});
+                start = start2;
+                end = end2;
             }
         }
 
-        if (intervals.length == 1) {
-            result.add(new int[]{start, end});
-        }
+        result.add(new int[]{start, end});
 
         return result.toArray(int[][]::new);
     }
