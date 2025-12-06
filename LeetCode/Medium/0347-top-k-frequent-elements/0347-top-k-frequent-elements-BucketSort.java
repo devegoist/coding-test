4c123cd9b08e246696be646f9fab26e016394421
김은수 <devegoist@gmail.com>
2025-12-06 19:35:51 +0900

    Time: 14 ms (64.42%), Space: 47.7 MB (59.86%) - LeetHub

---
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements-BucketSort.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+
+        int[] result = new int[k];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.offer(entry);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = pq.poll().getKey();
+        }
+
+        return result;
+    }
+}
