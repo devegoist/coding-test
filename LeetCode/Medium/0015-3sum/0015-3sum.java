1a11ff442b8df0faac61f5af50ea4f9bd5c7a6c4
김은수 <devegoist@gmail.com>
2025-12-11 22:10:04 +0900

    Time: 32 ms (61.08%), Space: 59.3 MB (19.99%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -1,38 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> result = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
 
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;  // 이전과 같은 값이면 건너뛰기
+            }
             int a = nums[i];
 
             int left = i + 1;
             int right = nums.length - 1;
 
-            while (left != right && left < right) {
+            while (left < right) {
                 int b = nums[left];
                 int c = nums[right];
-
-                if (b + c == -a) {
-                    List<Integer> sum = new ArrayList<>();
-                    sum.add(a);
-                    sum.add(b);
-                    sum.add(c);
-                    result.add(sum);
-                }
 
                 if (b + c > -a) {
                     right--;
                 } else if (b + c < -a) {
                     left++;
                 } else {
+                    result.add(List.of(a, b, c));
                     right--;
                     left++;
+
+                    while (left < right && nums[left] == b) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == c) {
+                        right--;
+                    }
                 }
             }
         }
 
-        return new ArrayList<>(result);
+        return result;
     }
 }
