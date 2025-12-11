8dc730c2c009db58c7a18d0c7c21f2b225e9b4ed
김은수 <devegoist@gmail.com>
2025-12-11 17:33:24 +0900

    Time: 2 ms (98.92%), Space: 44.6 MB (57.51%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] map = new int[27];
+        int[] map = new int[26];
 
         for (char c : s.toCharArray()) {
-            map[c - 96] += 1;
+            map[c - 'a'] += 1;
         }
 
         for (char c : t.toCharArray()) {
-            map[c - 96] -= 1;
+            map[c - 'a'] -= 1;
         }
 
         for (int m : map) {
