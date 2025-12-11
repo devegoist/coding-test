b951cdcf109e3cc413f2b283a7f4c99a69884dbb
김은수 <devegoist@gmail.com>
2025-12-11 16:53:20 +0900

    Time: 0 ms (100%), Space: 45.7 MB (65.64%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,15 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        System.gc();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (i != j
-                    && target - nums[i] == nums[j]) {
-                    return new int[]{i, j};
-                }
+        for(int i = 1; i < nums.length; i++){
+            for(int j = i; j < nums.length; j++){
+                if(nums[j] + nums[j-i] == target)
+                    return new int[]{j, j-i};
             }
         }
-
-        return null;
+        return new int[]{-1, -1};
     }
 }
