8f5d83f98e01f580720a60599b547aec50fa07d4
김은수 <devegoist@gmail.com>
2025-12-11 19:13:12 +0900

    Time: 3 ms (78.02%), Space: 43.5 MB (15.1%) - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,11 +3,11 @@ class Solution {
         Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
-            if ('(' == c) {
+            if (c == '(') {
                 stack.push(')');
-            } else if ('{' == c) {
+            } else if (c == '{') {
                 stack.push('}');
-            } else if ('[' == c) {
+            } else if (c == '[') {
                 stack.push(']');
             } else {
                 if (stack.isEmpty() || stack.pop() != c) {
@@ -15,7 +15,7 @@ class Solution {
                 }
             }
         }
-
+        
         return stack.isEmpty();
     }
 }
