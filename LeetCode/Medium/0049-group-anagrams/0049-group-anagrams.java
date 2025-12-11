a68d440d643ce6dbbcfe407b00358ea651f4c918
김은수 <devegoist@gmail.com>
2025-12-11 17:52:59 +0900

    Time: 6 ms (98.89%), Space: 49.3 MB (58.86%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
@@ -1,6 +1,5 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-
         Map<String, List<String>> map = new HashMap<>();
         for (String str : strs) {
             char[] c = str.toCharArray();
