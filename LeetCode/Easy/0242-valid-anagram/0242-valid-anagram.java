7afbc0553e34cf0b3711ac28b80d9f0105240367
김은수 <devegoist@gmail.com>
2025-12-11 17:33:20 +0900

    Time: 2 ms (98.92%), Space: 44.6 MB (53.15%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -1,30 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> map1 = new HashMap<>();
+        int[] map = new int[27];
+
         for (char c : s.toCharArray()) {
-            map1.put(c, map1.getOrDefault(c, 0) + 1);
+            map[c - 96] += 1;
         }
 
-        Map<Character, Integer> map2 = new HashMap<>();
         for (char c : t.toCharArray()) {
-            map2.put(c, map2.getOrDefault(c, 0) + 1);
-        }
-        
-        if (map1.size() != map2.size()) {
-            return false;
-        }
-        
-        for (Map.Entry<Character, Integer> entry : map1.entrySet()) {
-            System.out.println(1);
-            if (!map2.containsKey(entry.getKey())) {
-                return false;
-            }
-            
-            if (!entry.getValue().equals(map2.get(entry.getKey()))) {
-                return false;
-            }
+            map[c - 96] -= 1;
         }
 
+        for (int m : map) {
+            if (m != 0) {
+                return false;
+            }
+        }
+        
         return true;
     }
 }
