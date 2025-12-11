d8a32274845bf99fd51042e9796189a4f09720a6
김은수 <devegoist@gmail.com>
2025-12-11 16:48:16 +0900

    Time: 45 ms (28.96%), Space: 46.3 MB (55.65%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,20 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // 2, 7, 11, 15
-        // 9
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            
-            if(map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (i != j
+                    && target - nums[i] == nums[j]) {
+                    return new int[]{i, j};
+                }
             }
-            
-            map.put(nums[i], i);
         }
 
-        return new int[]{};
+        return null;
     }
 }
