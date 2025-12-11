5479a00030322d82aa7c8bc8af6854abc5b7ed4d
김은수 <devegoist@gmail.com>
2025-12-11 17:05:19 +0900

    Time: 22 ms (22.23%), Space: 94.9 MB (16.01%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,21 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i+1]) {
+        // Arrays.sort(nums); // nLogn
+        // for (int i = 0; i < nums.length - 1; i++) {
+        //     if (nums[i] == nums[i+1]) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
                 return true;
             }
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+
         return false;
     }
 }
