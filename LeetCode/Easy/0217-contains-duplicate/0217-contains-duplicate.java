1e29bddf56e5b7786eebfe72233f624506df9afe
김은수 <devegoist@gmail.com>
2025-12-11 17:06:42 +0900

    Time: 10 ms (96.97%), Space: 97.5 MB (8.66%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -18,10 +18,9 @@ class Solution {
 
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
-            if (set.contains(num)) {
+            if (!set.add(num)) {
                 return true;
             }
-            set.add(num);
         }
 
         return false;
