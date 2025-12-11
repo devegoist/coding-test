f492d8b2b6e73e3b7b3ff02929be225e1b4ddc4d
김은수 <devegoist@gmail.com>
2025-12-11 20:39:58 +0900

    Time: 406 ms (5.38%), Space: 48.2 MB (50.01%) - LeetHub

--- LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         for (int i = 0; i < numbers.length - 1; i++) {
-            int diff = target - numbers[i];
             for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[j] == diff) {
+                if (numbers[i] + numbers[j] == target) {
                     return new int[]{i + 1, j + 1};
                 }
             }
