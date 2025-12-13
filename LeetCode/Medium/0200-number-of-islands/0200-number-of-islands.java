88008105b376bdd09b6959e957f46898d5d0b62e
김은수 <devegoist@gmail.com>
2025-12-13 19:34:43 +0900

    Time: 4 ms (48.46%), Space: 52.4 MB (24%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -2,19 +2,17 @@ class Solution {
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        
         boolean[][] visited = new boolean[m][n];
         int result = 0;
 
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    result++;
-                    visited[i][j] = true;
+                if (visited[i][j] == false && grid[i][j] == '1') {
                     dfs(grid, visited, i, j, dx, dy);
+                    result++;
                 }
             }
         }
@@ -22,19 +20,21 @@ class Solution {
         return result;
     }
 
-    private void dfs(char[][] grid, boolean [][] visited, int x, int y, int[] dx, int[] dy) {
+    private void dfs(char[][] grid, boolean[][] visited, int m, int n, int[] dx, int[] dy) {
+        visited[m][n] = true;
+
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0
-                && ny >= 0
-                && nx < grid.length
+            int nx = m + dx[i];
+            int ny = n + dy[i];
+
+            if (nx >= 0 
+                && ny >= 0 
+                && nx < grid.length 
                 && ny < grid[0].length
-                && !visited[nx][ny]
-                && grid[nx][ny] == '1') {
-                    visited[nx][ny] = true;
-                    dfs(grid, visited, nx, ny, dx, dy);
-            }
+                && grid[nx][ny] == '1'
+                && !visited[nx][ny]) {
+                dfs(grid, visited, nx, ny, dx, dy);
+            }           
         }
     }
 }
