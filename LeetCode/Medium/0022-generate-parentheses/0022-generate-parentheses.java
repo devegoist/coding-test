82fbedd2599a36bf76f2c14d3857fdd05af98a1e
김은수 <devegoist@gmail.com>
2025-12-14 21:23:53 +0900

    Time: 1 ms (83.55%), Space: 44.6 MB (45.3%) - LeetHub

--- LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.java
+++ LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        process(n, 0, 0, "", result);
+        StringBuilder sb = new StringBuilder();
+        process(n, 0, 0, sb, result);
         return result;
     }
 
-    private void process(int n, int open, int closed, String str, List<String> result) {
+    private void process(int n, int open, int closed, StringBuilder sb, List<String> result) {
         if (open == n && closed == n) {
-            result.add(str);
+            result.add(sb.toString());
             return;
         }
 
         if (open <= n) {
-            process(n, open+1, closed, str+"(", result); //open
+            process(n, open+1, closed, sb.append("("), result); //open
+            sb.deleteCharAt(sb.length()-1);
         }
         if (closed < open) {
-            process(n, open, closed+1, str+")", result); //closed
+            process(n, open, closed+1, sb.append(")"), result); //closed
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
