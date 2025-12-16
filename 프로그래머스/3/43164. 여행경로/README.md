67098bd53d0b461b80af6dffaaa5910a3451de1d
김은수 <devegoist@gmail.com>
2025-12-16 21:36:55 +0900

    [level 3] Title: 여행경로, Time: 13.47 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java) 
 
 ### 성능 요약
 
-메모리: 75.6 MB, 시간: 2.33 ms
+메모리: 95.9 MB, 시간: 13.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 05일 12:39:52
+2025년 12월 16일 21:36:50
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,46 +1,43 @@
 import java.util.*;
 
 class Solution {
-    private List<String> result = new ArrayList<>();
-    private boolean[] used;
+    private boolean[] visited;
+    private List<String> path;
     
     public String[] solution(String[][] tickets) {
-        used = new boolean[tickets.length];
+        String[] result = {};
         
-        // 출발지별로 도착지를 알파벳 순 정렬
         Arrays.sort(tickets, (a, b) -> {
             if (a[0].equals(b[0])) {
-                return a[1].compareTo(b[1]); // 도착지 비교
+                return a[1].compareTo(b[1]);
             }
-            return a[0].compareTo(b[0]); // 출발지 비교
+            return a[0].compareTo(b[0]);
         });
+        visited = new boolean[tickets.length];
+        path = new ArrayList<>();
+        path.add("ICN");
         
-        result.add("ICN");
+        dfs(tickets, "ICN");
         
-        tour(tickets, "ICN");
-        
-        return result.stream()
-            .toArray(String[]::new);
+        return path.toArray(String[]::new);
     }
     
-    private boolean tour(String[][] tickets, String country) {
-        if (result.size() == tickets.length + 1) {
+    private boolean dfs(String[][] tickets, String current) {
+        if (path.size() == tickets.length + 1) {
             return true;
         }
         
-        // List<String[]> destinations = new ArrayList<>();
         for (int i = 0; i < tickets.length; i++) {
-            if (country.equals(tickets[i][0])
-               && !used[i]) {
-                used[i] = true;
-                result.add(tickets[i][1]);
-
-                if (tour(tickets, tickets[i][1])) {
+            if (!visited[i] && current.equals(tickets[i][0])) {
+                System.out.println(current + " " + tickets[i][0] + " " + tickets[i][1]);
+                visited[i] = true;
+                String next = tickets[i][1];
+                path.add(next);
+                if (dfs(tickets, next)) {
                     return true;
                 }
-                
-                used[i] = false;
-                result.remove(result.size() - 1);
+                path.remove(path.size() - 1);
+                visited[i] = false;
             }
         }
         
