393e40916c9557245d7cebfa48c66d792f5eb41e
김은수 <devegoist@gmail.com>
2025-12-16 19:34:28 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 86.2 MB, 시간: 1.40 ms
+메모리: 86.9 MB, 시간: 0.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 06일 17:34:00
+2025년 12월 16일 19:34:25
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,46 +1,29 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] computers) {
-        // 너비우선탐색으로 풀어보자
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (computers[i][j] == 1) {
-                    if (!graph.containsKey(i)) {
-                        graph.put(i, new ArrayList<>());
-                    }
-                    graph.get(i).add(j);    
-                }
-            }
-        }
-        boolean[] visited = new boolean[n+1];
+        int answer = 0;
+        boolean[] visited = new boolean[n];
         
-        // 0, [0, 1]
-        // 1, [1, 0]
-        // 2, [2]
-        
-        int count = 0;
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                count++;
-                
-                Queue<Integer> queue = new LinkedList<>();
-                visited[i] = true;
-                queue.add(i);
-
-                while (!queue.isEmpty()) {
-                    int computer = queue.poll();
-                    for (int linked : graph.get(computer)) {
-                        if (!visited[linked]) {
-                            visited[linked] = true;
-                            queue.add(linked);
-                        }
-                    }
-                }  
-            }  
+                dfs(computers, i, visited);    
+                answer++;
+            }
         }
         
-        return count;
+        return answer;
+    }
+    // ... 0
+    // ... 1
+    
+    
+    
+    private void dfs(int[][] computers, int current, boolean[] visited) {
+        visited[current] = true;
+        
+        for (int i = 0; i < computers[current].length; i++) {
+            if (i != current && !visited[i] && computers[current][i] == 1) {
+                dfs(computers, i, visited);    
+            }
+        }
     }
 }
