1af295f88da28ae3f2663e4648823bbc06a7e108
김은수 <devegoist@gmail.com>
2025-12-17 00:47:42 +0900

    [level 3] Title: 등굣길, Time: 0.55 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
 
 ### 성능 요약
 
-메모리: 53.8 MB, 시간: 0.59 ms
+메모리: 54.1 MB, 시간: 0.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 27일 11:33:33
+2025년 12월 17일 00:47:36
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,40 +1,39 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] routes = new int[n + 1][m + 1];
+        int answer = 0;
+        int[][] dp = new int[m][n];
         
-        // 물 웅덩이 세팅
         for (int[] puddle: puddles) {
-            routes[puddle[1]][puddle[0]] = -1;
+            dp[puddle[0]-1][puddle[1]-1] = -1;
         }
         
-        // 기본값 세팅
-        routes[1][1] = 1;
-        for (int i = 2; i <= n; i++) {
-            if (routes[i][1] == -1) {
-                routes[i][1] = 0;
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            if (dp[i][0] != -1) {
+                dp[i][0] = dp[i-1][0];    
             } else {
-                routes[i][1] = routes[i-1][1];
+                dp[i][0] = 0;
             }
         }
         
-        for (int i = 2; i <= m; i++) {
-            if (routes[1][i] == -1) {
-                routes[1][i] = 0;
+        for (int i = 1; i < n; i++) {
+            if (dp[0][i] != -1) {
+                dp[0][i] = dp[0][i-1];
             } else {
-                routes[1][i] = routes[1][i-1];
+                dp[0][i] = 0;
             }
         }
         
-        for (int i = 2; i <= n; i++) {
-            for (int j = 2; j <= m; j++) {
-                if (routes[i][j] == -1) {
-                    routes[i][j] = 0;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (dp[i][j] != -1) {
+                    dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1_000_000_007;
                 } else {
-                    routes[i][j] = (routes[i-1][j] + routes[i][j-1]) % 1_000_000_007;
+                    dp[i][j] = 0;
                 }
             }
         }
         
-        return routes[n][m];
+        return dp[m-1][n-1];
     }
 }
