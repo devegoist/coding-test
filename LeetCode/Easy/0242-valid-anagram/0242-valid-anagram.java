4b1931ef045413c60e21e7db0c765945a8aecf6f
김은수 <devegoist@gmail.com>
2026-01-05 16:56:15 +0900

    Time: 14 ms (27.26%), Space: 47 MB (5.64%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -4,22 +4,32 @@ class Solution {
             return false;
         }
 
-        int[] map = new int[26];
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
 
-        for (char c : s.toCharArray()) {
-            map[c - 'a'] += 1;
+        for (Character c: s.toCharArray()) {
+            if (!map1.containsKey(c)) {
+                map1.put(c, 1);
+            }
+            map1.put(c, map1.get(c) + 1);
         }
 
-        for (char c : t.toCharArray()) {
-            map[c - 'a'] -= 1;
+        for (Character c: t.toCharArray()) {
+            if (!map2.containsKey(c)) {
+                map2.put(c, 1);
+            }
+            map2.put(c, map2.get(c) + 1);
         }
 
-        for (int m : map) {
-            if (m != 0) {
+        for (Map.Entry<Character, Integer> entry: map1.entrySet()) {
+            char key = entry.getKey();
+            int val = entry.getValue();
+            if (!(map2.containsKey(key) && val == map2.get(key))) {
+                
                 return false;
             }
         }
-        
+
         return true;
     }
 }
