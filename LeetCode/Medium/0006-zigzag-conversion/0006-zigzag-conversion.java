9b59d68a1fb375918ff3fa61065c28aaceb449ee
김은수 <devegoist@gmail.com>
2026-01-07 00:01:50 +0900

    Time: 15 ms (10.12%), Space: 47.7 MB (9.64%) - LeetHub

--- LeetCode/Medium/0006-zigzag-conversion/0006-zigzag-conversion.java
+++ LeetCode/Medium/0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,5 +1,9 @@
 class Solution {
     public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        
         StringBuilder[] rows = new StringBuilder[numRows];
         for (int i = 0; i < numRows; i++) {
             rows[i] = new StringBuilder();
