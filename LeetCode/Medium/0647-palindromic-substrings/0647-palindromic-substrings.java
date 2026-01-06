c3909fdf1e22a5d911e89d6c0ecc44fe03df2ae3
김은수 <devegoist@gmail.com>
2026-01-06 11:22:14 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.java
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,23 +1,31 @@
 class Solution {
-    private int count = 0;
     public int countSubstrings(String s) {
-        int length = s.length();
-
-        for (int i = 0; i < length; i++) {
-            search(s, i, i);
-            search(s, i, i+1);
+        int result = 0;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = true;
         }
 
-        return count;
-    }
-
-    private void search(String s, int left, int right) {
-        while(left >= 0 
-            && right < s.length() 
-            && s.charAt(left) == s.charAt(right)) {
-            count++;
-            left--;
-            right++;
+        for (int i = 0; i < s.length()-1; i++) {
+            if (s.charAt(i) == s.charAt(i+1)) {
+                dp[i][i+1] = true;
+            }
         }
+
+        for (int i = 0; i < s.length()-2; i++) {
+            for (int j = i + 2; j < s.length(); j++) {
+                dp[i][j] = dp[i+1][j-1] && s.charAt(i) == s.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if (dp[i][j] == true) {
+                    result+=1;
+                }
+            }
+        }
+
+        return result;
     }
 }
