6af8a1fb77f25fc7b0244f4a178599f80e3efb68
김은수 <devegoist@gmail.com>
2026-01-07 23:05:27 +0900

    Time: 2 ms (98.82%), Space: 44.5 MB (55.5%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
 
-        StringBuilder sb = new StringBuilder();
+        while (left < right) {
+            while (left < right && !isAlphaNumeric(s.charAt(left))) {
+                left++;
+            }
 
-        for (char c : s.toCharArray()) {
-            if (isAlphaNumeric(c)) {
-                sb.append(c);
+            while (left < right && !isAlphaNumeric(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            } else {
+                left++;
+                right--;
             }
         }
-
-        String s1 = sb.toString();
-        String s2 = sb.reverse().toString();
-
-        return s1.equals(s2);
+        return true;
     }
 
     private boolean isAlphaNumeric(char c) {
-        return ('a' <= c && c <= 'z') || ('0' <= c && c <= '9');
+        return ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9');
     }
 }
