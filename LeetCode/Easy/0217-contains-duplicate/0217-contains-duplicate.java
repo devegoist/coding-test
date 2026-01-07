dba091b25288222f00845c1ea8e7268c3c9bac1f
김은수 <devegoist@gmail.com>
2026-01-07 23:12:06 +0900

    Time: 10 ms (99.19%), Space: 98.1 MB (7.58%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,28 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // Arrays.sort(nums); // nLogn
-        // for (int i = 0; i < nums.length - 1; i++) {
-        //     if (nums[i] == nums[i+1]) {
-        //         return true;
-        //     }
-        // }
-        // return false;
+        Set<Integer> set = new HashSet<>();
 
-        // Map<Integer, Integer> map = new HashMap<>(); O(n)
-        // for (int num : nums) {
-        //     if (map.containsKey(num)) {
-        //         return true;
-        //     }
-        //     map.put(num, map.getOrDefault(num, 0) + 1);
-        // }
-
-        Set<Integer> set = new HashSet<>(); // O(n)
-        for (int num : nums) {
+        for (int num: nums) {
             if (!set.add(num)) {
                 return true;
             }
         }
-
         return false;
     }
 }
