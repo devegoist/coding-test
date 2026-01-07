a6c9a5f09961b519dfa7cb293a27a670716041b6
김은수 <devegoist@gmail.com>
2026-01-07 22:30:55 +0900

    Time: 4 ms (51.44%), Space: 45.4 MB (39.77%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // 수박이박수
-        String condition = "abcdefghijklmnopqrstuvwxyz0123456789";
-
         s = s.toLowerCase();
 
         StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (isAlphanumeric(c)) {
+
+        for (char c : s.toCharArray()) {
+            if (isAlphaNumeric(c)) {
                 sb.append(c);
             }
         }
 
-        String a = sb.toString();
-        String b = sb.reverse().toString();
+        String s1 = sb.toString();
+        String s2 = sb.reverse().toString();
 
-        return a.equals(b);
+        return s1.equals(s2);
     }
 
-    private boolean isAlphanumeric(char c) {
-    return (c >= '0' && c <= '9') ||
-           (c >= 'a' && c <= 'z');
-}
+    private boolean isAlphaNumeric(char c) {
+        return ('a' <= c && c <= 'z') || ('0' <= c && c <= '9');
+    }
 }
