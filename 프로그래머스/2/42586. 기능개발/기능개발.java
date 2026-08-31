f78c1d9db1344909823d5f05e9be274341d81f3a
김은수 <devegoist@gmail.com>
2026-08-31 23:14:08 +0900

    [level 2] Title: 기능개발, Time: 2.51 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,36 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // 7, 3, 9
-        
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < progresses.length; i++) {
-            int progress = progresses[i];
-            int days = (100 - progress) / speeds[i];
-            if ((100 - progress) % speeds[i] > 0) {
-                days++;
-            }
-            queue.offer(days);
+        List<Integer> days = new ArrayList<>();
+        for (int i=0; i<progresses.length; i++) {
+            days.add(((100 - progresses[i]) + (speeds[i] - 1) ) / speeds[i]);
         }
         
         List<Integer> result = new ArrayList<>();
-        int current = queue.poll();
-        int count = 1;
-        while (!queue.isEmpty()) {
-            int peek = queue.peek();
-            if (peek <= current) {
-                queue.poll();
-                count++;
+        int cnt = 1;
+        int day = days.get(0);
+        
+        for (int i=1; i<days.size(); i++) {
+            if (day >= days.get(i)) {
+                cnt++;
             } else {
-                result.add(count);
-                current = queue.poll();
-                count = 1;
-            }
-            
-            if (queue.isEmpty()) {
-                result.add(count);
+                result.add(cnt);
+                day = days.get(i);
+                cnt = 1;
             }
         }
+        result.add(cnt);
         
         return result.stream()
             .mapToInt(Integer::intValue)
